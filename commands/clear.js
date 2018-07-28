de2eedab4a11c89276474e3f9a5c80d9d82b644e
JCoDog <jayconnolly14@icloud.com>
2018-07-28 21:39:59 +0100

    Update
    End of development on 28/07/2018 @ 21:39

--- commands/clear.js
+++ commands/clear.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
   if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("OOF. You do not appear to have access to this command, you need to be able to manage messages to do this!");
   if (!args[0]) return message.reply("You did not specify the amount to delete");
-  let x = args[0]+1;
+  let x = parseInt(args[0])+1;
   message.channel.bulkDelete(x).then(() => {
     message.channel.send(`Cleared ${args[0]} messaged from this chat.`).then(msg => msg.delete(5000));
   });
