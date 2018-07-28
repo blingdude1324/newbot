59b2c5119a95627fdaaa157cb36f06916f1b3879
JCoDog <jayconnolly14@icloud.com>
2018-07-28 19:37:55 +0100

    Unpdate

--- bot.js
+++ bot.js
@@ -1,14 +1,18 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const config = require("./botconfig.json");
+
+const prefix = botconfig.prefix;
+
+client.login(process.env.token);
 
 client.on('ready', () => {
   console.log("SolBot Activated.");
+  client.user.setStatus("away", "In Development")
 });
 
 client.on('message', message => {
   if (message.content === 'stats') {
-    message.reply('Currently in development of version 0.0.1');
+    message.reply('I am currently in development for the version 1 release in September');
   }
 });
-
-client.login(process.env.token);
